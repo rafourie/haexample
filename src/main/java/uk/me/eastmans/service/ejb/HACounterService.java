6d01e0ec3afc8f01602419c26dce88a2ac608fba
markeastman <meastman@redhat.com>
2016-08-11 15:25:44 +0100

    Added HA singleton service

---
+++ src/main/java/uk/me/eastmans/service/ejb/HACounterService.java
@@ -0,0 +1,44 @@
+package uk.me.eastmans.service.ejb;
+
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by markeastman on 11/08/2016.
+ */
+public class HACounterService implements Service<String> {
+    public static final ServiceName SINGLETON_SERVICE_NAME = ServiceName.JBOSS.append("openshift", "ha", "singleton", "counter");
+    /**
+     * A flag whether the service is started.
+     */
+    private final AtomicBoolean started = new AtomicBoolean(false);
+
+    /**
+     * @return the name of the server node
+     */
+    public String getValue() throws IllegalStateException, IllegalArgumentException {
+
+        return System.getenv("HOSTNAME");
+    }
+
+    public void start(StartContext arg0) throws StartException {
+        if (!started.compareAndSet(false, true)) {
+            throw new StartException("The service is still started!");
+        }
+        // Nothing to really start
+    }
+
+    public void stop(StopContext arg0) {
+        if (started.compareAndSet(true, false)) {
+            // Nothing to really stop
+        }
+    }
+
+}
