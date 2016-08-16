33357df762955f98de436e3f556c6c5804a5b015
markeastman <meastman@redhat.com>
2016-08-16 14:46:22 +0100

    Changed lookup name in servlet code

--- src/main/java/uk/me/eastmans/service/ejb/GlobalCounter.java
+++ src/main/java/uk/me/eastmans/service/ejb/GlobalCounter.java
@@ -5,7 +5,6 @@ import javax.ejb.Remote;
 /**
  * Created by markeastman on 11/08/2016.
  */
-@Remote
 public interface GlobalCounter {
     void reset();
     int increment();
--- src/main/java/uk/me/eastmans/service/ejb/GlobalCounterBean.java
+++ src/main/java/uk/me/eastmans/service/ejb/GlobalCounterBean.java
@@ -2,12 +2,14 @@ package uk.me.eastmans.service.ejb;
 
 import javax.ejb.Lock;
 import javax.ejb.LockType;
+import javax.ejb.Remote;
 import javax.ejb.Singleton;
 import java.util.logging.Logger;
 
 /**
  * Created by markeastman on 11/08/2016.
  */
+@Remote
 @Singleton
 public class GlobalCounterBean implements GlobalCounter {
 
@@ -21,15 +23,11 @@ public class GlobalCounterBean implements GlobalCounter {
         Thread.dumpStack();
     }
 
-    @Override
-    @Lock(LockType.WRITE)
     public void reset()
     {
         counter = 0;
     }
 
-    @Override
-    @Lock(LockType.WRITE)
     public int increment ()
     {
         return ++counter;
