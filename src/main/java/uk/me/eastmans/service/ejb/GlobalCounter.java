915617e435549811d5e978ecfcee53c46683c89e
markeastman <meastman@redhat.com>
2016-08-17 13:43:06 +0100

    Tidied up the actual GlobalCounterBean

--- src/main/java/uk/me/eastmans/service/ejb/GlobalCounter.java
+++ src/main/java/uk/me/eastmans/service/ejb/GlobalCounter.java
@@ -1,11 +1,8 @@
 package uk.me.eastmans.service.ejb;
 
-import javax.ejb.Remote;
-
 /**
  * Created by markeastman on 11/08/2016.
  */
-@Remote
 public interface GlobalCounter {
     void reset();
     int increment();
--- src/main/java/uk/me/eastmans/service/ejb/GlobalCounterBean.java
+++ src/main/java/uk/me/eastmans/service/ejb/GlobalCounterBean.java
@@ -2,31 +2,22 @@ package uk.me.eastmans.service.ejb;
 
 import javax.ejb.Lock;
 import javax.ejb.LockType;
-import javax.ejb.Remote;
 import javax.ejb.Singleton;
-import java.util.logging.Logger;
 
 /**
  * Created by markeastman on 11/08/2016.
  */
 @Singleton
-@Remote (GlobalCounter.class)
 public class GlobalCounterBean implements GlobalCounter {
-
-    private final Logger log = Logger.getLogger(this.getClass().toString());
-
     private int counter = 0;
 
-    public GlobalCounterBean()
-    {
-        log.info( "+++++++++++++ GlobalCounterBean created" );
-    }
-
+    @Lock(LockType.WRITE)
     public void reset()
     {
         counter = 0;
     }
 
+    @Lock(LockType.WRITE)
     public int increment ()
     {
         return ++counter;
