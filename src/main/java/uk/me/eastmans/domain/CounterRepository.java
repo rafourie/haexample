30593828bb1ebae3bef2ce19ec84b3e2e1ea4f27
markeastman <meastman@redhat.com>
2016-09-05 11:47:36 +0100

    Added @Transactional on method

--- src/main/java/uk/me/eastmans/domain/CounterRepository.java
+++ src/main/java/uk/me/eastmans/domain/CounterRepository.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 /**
  * Created by markeastman on 23/08/2016.
@@ -15,6 +16,7 @@ public class CounterRepository {
     @Inject
     private EntityManager em;
 
+    @Transactional
     public int incrementCounter( String name )
     {
         TypedQuery<Counter> query = em.createQuery( "select c from Counter c where c.name = :name", Counter.class);
--- src/main/resources/bootstrap.sql
+++ src/main/resources/bootstrap.sql
@@ -1,1 +1,1 @@
-insert into counter (id, name, value) values (1, 'globalSingleton', 0);
+insert into counters (id, name, current_value) values (1, 'globalSingleton', 10);
